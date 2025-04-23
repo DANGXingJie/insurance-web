507693d9db8cd6ad01ffafa14e3426dc8a07389e
dangxingjie <1328791209@qq.com>
2025-04-23 12:31:53 +0800

    feat:完成音频播放器播放倍数\播放列表

--- src/view/podcast/index.vue
+++ src/view/podcast/index.vue
@@ -20,7 +20,7 @@
       <div class="columns-4 gap-8 space-y-8 mt-20">
         <template v-for="(item, index) in items" :key="index">
           <!-- 每个项目 -->
-          <div class="break-inside-avoid" :data-key="index">
+          <div class="break-inside-avoid hover:cursor-pointer" :data-key="index">
             <div class="bg-white rounded-l shadow-md">
               <div class="w-[278px] pt-4 pl-5 pr-4" :style="{ height: `${item.height}px` }">
                 <div class="relative h-[238px]">
@@ -32,7 +32,8 @@
                     </div>
                     <template v-else>
                       <div class="w-[68px] h-[28px] text-xs bg-gray-400 flex justify-center items-center">精选电台</div>
-                      <div class="w-[54px] h-[28px] text-xs flex items-center bg-black text-white">
+                      <div @click="handlePlayAudio(item)"
+                        class="w-[54px] h-[28px] text-xs flex items-center bg-black text-white">
                         <icon-play-arrow-fill size="20" />音频
                       </div>
                     </template>
@@ -44,7 +45,8 @@
                   {{ item.desc }}
                 </div>
                 <div class="flex items-center justify-between mt-4">
-                  <div class="flex text-xs items-center text-gray-400">作者 ·<span class="ml-2">{{ item.auther }}</span>
+                  <div class="flex text-xs items-center text-gray-400">
+                    作者 ·<span class="ml-2">{{ item.auther }}</span>
                   </div>
                   <div class="text-xs text-gray-400 flex justify-center items-center">
                     <icon-fire class="text-red-500" size="14" /><span class="ml-1">73.2万</span>
@@ -62,21 +64,23 @@
     </div>
   </div>
   <!-- 音频播放器 -->
-  <div class="fixed bottom-0 left-0 right-0 bg-gray-400 w-full h-[90px]">
-    <div class="w-[1300px]  m-auto h-full flex items-center">
+  <div v-if="isShowAudioPlayer" class="fixed bottom-0 left-0 right-0 bg-gray-400 w-full h-[90px]">
+    <div class="w-[1300px] m-auto h-full flex items-center">
       <!-- left播放控制 -->
       <div class="w-[160px] h-full flex items-center justify-around">
-        <icon-skip-previous-fill @click="handlePrevSong" style="color: white;" size="30" />
-        <icon-pause style="color: white;" v-if="isPlaying" @click="handlePlay" size="40" />
-        <icon-play-arrow-fill v-else @click="handlePlay" style="color: white;" size="40" />
-        <icon-skip-next-fill @click="handleNextSong" style="color: white;" size="30" />
+        <icon-skip-previous-fill @click="handlePrevSong" style="color: white" size="30" />
+        <icon-pause style="color: white" v-if="isPlaying" @click="handlePlay" size="40" />
+        <icon-play-arrow-fill v-else @click="handlePlay" style="color: white" size="40" />
+        <icon-skip-next-fill @click="handleNextSong" style="color: white" size="30" />
       </div>
       <!-- center播放进度条 -->
       <div class="flex-1 h-full bg-gray-400 flex items-center">
         <img class="ml-10 w-[68px] h-[68px]" src="@/assets/images/pubo.png" />
         <div>
           <div class="flex justify-between text-white ml-7">
-            <div class="text-xs font-medium mr-1"><span v-if="isPlaying">{{ currentSong.title }}</span></div>
+            <div class="text-xs font-medium mr-1">
+              <span v-if="isPlaying">{{ currentSong.title }}</span>
+            </div>
             <div>
               <span class="text-xs font-medium mr-1">{{ formatTime(currentTime) }}</span>
               <span class="text-xs font-medium">/</span>
@@ -88,11 +92,39 @@
         </div>
       </div>
       <!-- right 功能按钮 -->
-      <div class="w-[250px] h-full  flex justify-around items-center text-white hover:cursor-pointer">
-        <span>文稿</span>
-        <span>倍速</span>
-        <span>列表</span>
-        <icon-close-circle-fill style="color: #000;;" size="35" />
+      <div class="w-[250px] h-full relative flex justify-around items-center text-white hover:cursor-pointer">
+        <span @click="handleToPage">文稿</span>
+        <span @click="handlePlayMultiplier">倍速</span>
+        <span @click="handlePlayList">列表</span>
+        <icon-close-circle-fill @click="handleCloseAudioPlayer" style="color: #000" size="35" />
+        <!--播放倍数选择-->
+        <div v-if="showPlayMultiplier"
+          class="absolute -top-[160px] right-[120px] w-[73px] h-[162px] bg-[#222222] flex flex-col justify-around text-syGreyBg">
+          <div v-for="(item, index) in multipleList" class="w-full h-[30px] flex items-center justify-center"
+            :key="index">
+            <span :class="item.id === currentRate ? 'font-bold text-base text-white' : ''" class="text-grey text-sm"
+              @click="handleSelectPlayMultiplier(item)">{{ item.rate === 1.0 ? '正常语速' : `${item.rate}x` }}</span>
+          </div>
+        </div>
+        <!-- 播放列表 -->
+        <div v-if="showPlayList"
+          class="absolute -top-[149px] pt-5 pr-6 pl-5 right-[50px] w-[612px] max-h-[149px] bg-[#222222] text-syGreyBg overflow-y-auto">
+          <div class="flex justify-end w-full">
+            <icon-close @click="closePlaylist" size="26" style="color: #a3a3a3ff" />
+          </div>
+          <div class="mt-2 flex justify-between items-center">
+            <span class="text-sm">播放列表（{{ playlist.length }}）</span>
+            <button class="w-[50px] h-[26px] border rounded-[40px] border-gray-400 flex justify-center items-center">
+              清空
+            </button>
+          </div>
+          <div class="mt-2">
+            <div v-for="(item, index) in playlist" :key="index" class="flex justify-between items-center">
+              <div :data-key="item" class="truncate w-[300px] mb-2 text-xs">{{ item.title }}</div>
+              <div class="hover:cursor-pointer text-grey text-xs mr-2">删除</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -103,13 +135,15 @@ import listImg from '@/assets/images/pubo.png'
 import song1 from '@/assets/周杰伦-不能说的秘密.mp3'
 import song2 from '@/assets/周杰伦-退后.mp3'
 import song3 from '@/assets/周杰伦-最长的电影.mp3'
+import router from '@/router'
+//播客/课程数据
 const items = ref([
   {
     id: 1,
     title: '史蒂夫说424期 - 凉意 - 世界一流的亲密关系是什么样的？',
     desc: '史蒂夫说',
     type: 1,
-    auther: "史蒂夫",
+    auther: '史蒂夫',
     img: listImg,
     height: 443,
   },
@@ -118,7 +152,7 @@ const items = ref([
     type: 1,
     title: 'No.31 对话瑞哥：我带“甲亢哥”逛重庆',
     desc: '我有一个朋友|董晨宇·',
-    auther: "董晨宇",
+    auther: '董晨宇',
     img: listImg,
     height: 443,
   },
@@ -128,8 +162,8 @@ const items = ref([
     title: '101.【聊科技】机器人跑马拉松，意义何在？',
     desc: '请思考人形机器人在半马比赛中，可能出现哪些问题？为什么说这场半马比赛是一个难得的试验场？机器人跑马拉',
     img: listImg,
-    course: "给孩子的365天新闻课",
-    auther: "馒头星球",
+    course: '给孩子的365天新闻课',
+    auther: '馒头星球',
     height: 520,
   },
   {
@@ -137,8 +171,8 @@ const items = ref([
     title: '【导读】苗德岁：生命演化的历史与奥妙',
     desc: '大家好，我是苗德岁。很高兴担任这门《给孩子的365天文化课》的“生命简史”导师。欢迎大家选听这门课！',
     type: 2,
-    auther: "苗德岁",
-    course: "给孩子的365天文化课",
+    auther: '苗德岁',
+    course: '给孩子的365天文化课',
     img: listImg,
     height: 520,
   },
@@ -147,7 +181,7 @@ const items = ref([
     type: 1,
     title: '133.中产返贫：情绪、数据与事实｜青沙龙',
     desc: '问题青年',
-    auther: "问题青年",
+    auther: '问题青年',
     img: listImg,
     height: 443,
   },
@@ -181,25 +215,23 @@ const items = ref([
 ])
 
 // 播放列表数据
-const playlist = reactive(
-  [
-    {
-      title: '周杰伦-不能说的秘密',
-      artist: '艺术家1',
-      src: song1
-    },
-    {
-      title: '周杰伦-退后',
-      artist: '艺术家1',
-      src: song2
-    },
-    {
-      title: '周杰伦-最长的电影',
-      artist: '艺术家1',
-      src: song3
-    },
-  ]
-)
+const playlist = reactive([
+  {
+    title: '周杰伦-不能说的秘密',
+    artist: '艺术家1',
+    src: song1,
+  },
+  {
+    title: '周杰伦-退后',
+    artist: '艺术家1',
+    src: song2,
+  },
+  {
+    title: '周杰伦-最长的电影',
+    artist: '艺术家1',
+    src: song3,
+  },
+])
 //当前播放器播放歌曲索引
 const currentSongIndex = ref(0)
 //播放状态
@@ -230,47 +262,29 @@ const handlePlay = () => {
   }
   isPlaying.value = !isPlaying.value
 }
-const changeSong = (index) => {
-  currentSongIndex.value = index
-  if (isPlaying.value) {
-    audioElement.value.play()
-  }
-}
+// const changeSong = (index) => {
+//   currentSongIndex.value = index
+//   if (isPlaying.value) {
+//     audioElement.value.play()
+//   }
+// }
 // 下一首
 const handleNextSong = () => {
   currentSongIndex.value = (currentSongIndex.value + 1) % playlist.length
 }
 // 上一首
 const handlePrevSong = () => {
-  currentSongIndex.value =
-    (currentSongIndex.value - 1 + playlist.length) % playlist.length
+  currentSongIndex.value = (currentSongIndex.value - 1 + playlist.length) % playlist.length
 }
 
-// 用户拖动结束时的处理（Arco 推荐使用 @release 事件）
+//用户拖动进度条结束时的处理
 const handleSeek = (value: number) => {
-  console.log('%c [ 拖动结束 ]-253', 'font-size:13px; background:pink; color:#bf2c9f;',)
+  console.log('%c [ 拖动结束 ]-253', 'font-size:13px; background:pink; color:#bf2c9f;')
   if (audioElement.value) {
     audioElement.value.currentTime = value
     if (isPlaying.value) audioElement.value.play()
   }
 }
-
-//拖动进度条跳转播放位置
-// const seek = (e) => {
-//   if (!audioElement.value || duration.value <= 0) return
-//   const progressBar = e.currentTarget // 确保事件绑定在正确元素
-//   const rect = progressBar.getBoundingClientRect()
-
-//   // 边界约束
-//   let percentage = (e.clientX - rect.left) / rect.width
-//   percentage = Math.max(0, Math.min(1, percentage))
-//   // 检查音频可跳转状态
-//   if (audioElement.value.readyState >= HTMLMediaElement.HAVE_METADATA) {
-//     audioElement.value.currentTime = percentage * duration.value
-//   } else {
-//     console.warn('音频尚未加载完成')
-//   }
-// }
 // 时间格式化函数（处理小数）
 const formatTime = (seconds: number) => {
   const mins = Math.floor(seconds / 60)
@@ -324,21 +338,116 @@ onUnmounted(() => {
   }
 })
 
-</script>
+//handleToPage
+const handleToPage = () => {
+  // router.push({
+  //   path: '/detail',
+  //   query: {
+  //     id: '1',
+  //   },
+  // })
+}
+//播放速率控制
+const showPlayMultiplier = ref(false)
+const currentRate = ref(2)
+const multipleList = reactive([
+  {
+    id: 1,
+    rate: 0.7,
+  },
+  {
+    id: 2,
+    rate: 1.0,
+  },
+  {
+    id: 3,
+    rate: 1.25,
+  },
+  {
+    id: 4,
+    rate: 2.0,
+  },
+])
+// 播放倍速控制
+const handleSelectPlayMultiplier = (item: any) => {
+  console.log(item)
+  currentRate.value = item.id
+  if (audioElement.value) {
+    audioElement.value.playbackRate = item.rate
+  }
+}
+// 播放倍速显示
+const handlePlayMultiplier = () => {
+  showPlayMultiplier.value = !showPlayMultiplier.value
+  showPlayList.value = false
+}
+//播放列表
+const showPlayList = ref(false)
+//播放列表显示
+const handlePlayList = () => {
+  showPlayList.value = !showPlayList.value
+  showPlayMultiplier.value = false
+}
+//自定义指令
+// const vOutsideClick = {
+//   beforeMount: (el, binding) => {
+//     el.clickOutsideEvent = (event) => {
+//       if (!(el === event.target || el.contains(event.target))) {
+//         binding.value(event);
+//       }
+//     };
+//     document.addEventListener('click', el.clickOutsideEvent);
+//   },
+//   unmounted: (el) => {
+//     document.removeEventListener('click', el.clickOutsideEvent);
+//   },
+// };
 
+const closePlaylist = () => {
+  //如果播放列表是展开的，就设置关闭
+  if (showPlayList.value) {
+    showPlayList.value = false
+  }
+}
+
+//是否显示播放音频组件
+const isShowAudioPlayer = ref(false)
+//播放音频
+const handlePlayAudio = async (item: any) => {
+  if (audioElement.value) {
+    isShowAudioPlayer.value = true
+    try {
+      await audioElement.value.play();
+      //播放状态设置为true
+      isPlaying.value = true;
+      console.log('音频开始自动播放');
+    } catch (error) {
+      console.error('自动播放被阻止:', error);
+    }
+  }
+}
+//关闭底部音频组件
+const handleCloseAudioPlayer = () => {
+  isShowAudioPlayer.value = false
+  if (audioElement.value) {
+    audioElement.value.pause()
+    isPlaying.value = false
+  }
+}
+</script>
 <style scoped>
 /* 滚动条 样式 */
 :deep(.arco-slider-track::before) {
-  @apply h-[3px]
+  @apply h-[3px];
 }
 
 /* 前置样式 */
 :deep(.arco-slider-bar) {
-  @apply h-[3px] bg-red-600
+  @apply h-[3px] bg-red-600;
 }
 
 /* 进度条的点 */
 :deep(.arco-slider-btn::after) {
-  @apply border border-red-500
+  @apply border border-red-500;
 }
 </style>
